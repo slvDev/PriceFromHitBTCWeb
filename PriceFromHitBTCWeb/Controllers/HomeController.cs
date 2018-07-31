a8d0c8e4994e8e3112a6f71ac7db28cd1dff6230
Slava Serbov <blessod@gmail.com>
2018-07-31 09:26:34 +0300

    Added HomeController

---
+++ PriceFromHitBTCWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PriceFromHitBTCWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var returnPrice = GetCoinPrice();
+            return View(returnPrice);
+
+        }
+
+        public Coin GetCoinPrice()
+        {
+
+            var client = new WebClient();
+            string jsonData = client.DownloadString("https://api.hitbtc.com/api/2/public/ticker/BTCUSD");
+
+            var coin = new Coin();
+            coin = JsonConvert.DeserializeObject<Coin>(jsonData);
+
+            return coin;
+        }
+
+    }
+}
