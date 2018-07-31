d79483c0ad787d1e6a272baf5c54a192f17df577
Slava Serbov <blessod@gmail.com>
2018-07-31 09:33:47 +0300

    Added Index View

--- PriceFromHitBTCWeb/Controllers/HomeController.cs
+++ PriceFromHitBTCWeb/Controllers/HomeController.cs
@@ -5,14 +5,13 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using PriceFromHitBTCWeb.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PriceFromHitBTCWeb.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             var returnPrice = GetCoinPrice();
