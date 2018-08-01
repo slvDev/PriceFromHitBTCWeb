3b35c55ccff814058aebab0584c60c46b1632876
Slava Serbov <blessod@gmail.com>
2018-08-01 13:38:03 +0300

    Added coin properties for proper deserialization

--- PriceFromHitBTCWeb/Models/Coin.cs
+++ PriceFromHitBTCWeb/Models/Coin.cs
@@ -3,7 +3,16 @@ namespace PriceFromHitBTCWeb.Models
 {
     public class Coin
     {
-        public double Last { get; set; }
+        public string Ask { get; set; }
+        public string Bid { get; set; }
+        public string Last { get; set; }
+        public string Open { get; set; }
+        public string Low { get; set; }
+        public string High { get; set; }
+        public string Volume { get; set; }
+        public string VolumeQuote { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Symbol { get; set; }
 
     }
 }
